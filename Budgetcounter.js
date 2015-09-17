c8db83b1beb4d982e7f0dfd46e09410a5af6fc55
James R <kccreamkc@gmail.com>
2015-09-16 23:30:56 -0400

    Create Budgetcounter.js

---
+++ Budgetcounter.js
@@ -0,0 +1,22 @@
+function budgetdeductor(){
+
+if (Excatgory = entertainment)
+	entertainment.get("budget")
+
+	while (budget > 0)
+	{var temporary = budget;
+	
+		temporary -= "add jquery of user transaction amount input"
+		entertainment.set({budget: temporary});
+	}
+		
+
+	else
+	{
+		window.alert("you have exceeded your spending by "+(abs(temporary)+ "$")
+		<script>
+		document.getElementByID("name of pid here").innerHTML="you have exceeded your monthly budget for this category"
+		</script>;
+	}
+
+}
